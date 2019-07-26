21a5b3a4b72a8508a1b1edb57dcabfe00ff50233
Tanzim Mokammel <mtanzim@gmail.com>
2019-07-26 12:21:43 +0200

    cp: start hw6

--- hw6/hw6assignment.rb
+++ hw6/hw6assignment.rb
@@ -1,22 +1,66 @@
-# University of Washington, Programming Languages, Homework 6, hw6runner.rb
+  # University of Washington, Programming Languages, Homework 6, hw6runner.rb
 
-# This is the only file you turn in, so do not modify the other files as
-# part of your solution.
+  # This is the only file you turn in, so do not modify the other files as
+  # part of your solution.
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
-
+  AllMyPieces = [
+    [[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+    rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+    [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+    [[0, 0], [0, -1], [0, 1], [0, 2]]],
+    rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+    rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+    rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+    rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]),
+    rotations([[0, 0], [1, 0], [0, 1], [1, 1], [0,2] ]), #cube +1
+    rotations([[0, 0], [1, 0], [0, 1]]), # cube -1 
+    rotations([[0, 0], [-1, 0], [1, 0], [2, 0],[-2,0]]) #long +1
+  ] # Z
   # your enhancements here
+  def initialize(point_array, board)
+      super(point_array, board)
+  end
+
+  def self.next_piece (board)
+      MyPiece.new(AllMyPieces.sample, board)
+  end
 
 end
 
 class MyBoard < Board
   # your enhancements here
+  def initialize(game)
+      super(game)
+      @current_block = MyPiece.next_piece(self)
+  end
+
+  def next_piece
+      @current_block = MyPiece.next_piece(self)
+      @current_pos = nil
+  end
 
 end
 
 class MyTetris < Tetris
   # your enhancements here
+
+  def set_board
+      @canvas = TetrisCanvas.new
+      @board = MyBoard.new(self)
+      @canvas.place(@board.block_size * @board.num_rows + 3,
+                  @board.block_size * @board.num_columns + 6, 24, 80)
+      @board.draw
+  end
+
+  def key_bindings  
+      super
+      @root.bind('u' , proc {
+      @board.rotate_clockwise
+      @board.rotate_clockwise
+      })
+  end
 
 end
 
