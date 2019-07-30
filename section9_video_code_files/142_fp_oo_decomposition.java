2a27d92422de76207d44d65c2bab69ee39c68f18
Tanzim Mokammel <mtanzim@gmail.com>
2019-07-30 15:07:22 +0200

    format java code

--- section9_video_code_files/142_fp_oo_decomposition.java
+++ section9_video_code_files/142_fp_oo_decomposition.java
@@ -3,7 +3,9 @@
 
 abstract class Exp {
     abstract Value eval(); // no argument because no environment
+
     abstract String toStrng(); // renaming b/c toString in Object is public
+
     abstract boolean hasZero();
 }
 
@@ -15,54 +17,66 @@ abstract class Value extends Exp {
 
 class Int extends Value {
     public int i;
+
     Int(int i) {
-	this.i = i;
+        this.i = i;
     }
+
     Value eval() {
-	return this;
+        return this;
     }
+
     String toStrng() {
-	return "" + i;
+        return "" + i;
     }
+
     boolean hasZero() {
-	return i==0;
+        return i == 0;
     }
 }
 
 class Negate extends Exp {
     public Exp e;
+
     Negate(Exp e) {
-	this.e = e;
+        this.e = e;
     }
+
     Value eval() {
-	// we downcast from Exp to Int, which will raise a run-time error
-	// if the subexpression does not evaluate to an Int
-	return new Int(- ((Int)(e.eval())).i);
+        // we downcast from Exp to Int, which will raise a run-time error
+        // if the subexpression does not evaluate to an Int
+        return new Int(-((Int) (e.eval())).i);
     }
+
     String toStrng() {
-	return "-(" + e.toStrng() + ")";
+        return "-(" + e.toStrng() + ")";
     }
+
     boolean hasZero() {
-	return e.hasZero();
+        return e.hasZero();
     }
 }
 
 class Add extends Exp {
     Exp e1;
     Exp e2;
+
     Add(Exp e1, Exp e2) {
-	this.e1 = e1;
-	this.e2 = e2;
+        this.e1 = e1;
+        this.e2 = e2;
     }
+
     Value eval() {
-	// we downcast from Exp to Int, which will raise a run-time error
-	// if either subexpression does not evaluate to an Int
-	return new Int(((Int)(e1.eval())).i + ((Int)(e2.eval())).i);
+        // we downcast from Exp to Int, which will raise a run-time error
+        // if either subexpression does not evaluate to an Int
+        return new Int(((Int) (e1.eval())).i + ((Int) (e2.eval())).i);
     }
+
     String toStrng() {
-	return "(" + e1.toStrng() + " + " + e2.toStrng() + ")";
+        return "(" + e1.toStrng() + " + " + e2.toStrng() + ")";
     }
+
     boolean hasZero() {
-	return e1.hasZero() || e2.hasZero();
+        return e1.hasZero() || e2.hasZero();
     }
 }
